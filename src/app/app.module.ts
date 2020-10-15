19a43ea6a27990f50da3ea74b148f469a9d4b556
Warlen Pinheiro <warlen.pinheiro@empresta.com.br>
2020-10-15 20:56:33 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
